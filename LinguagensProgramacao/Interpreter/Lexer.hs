415ef0e3d106d6f19044d4dc73618185723a933c
dreyrobert <dreyrobert@gmail.com>
2024-05-20 13:40:21 -0300

    LP

--- LinguagensProgramacao/Interpreter/Lexer.hs
+++ LinguagensProgramacao/Interpreter/Lexer.hs
@@ -2,14 +2,18 @@ module Lexer where
 
 data Expr = BTrue
           | BFalse 
-          | Num Int 
+          | Num Int
+          | Var String
+          | Lam String Ty Expr
+          | App Expr Expr
           | Add Expr Expr 
           | And Expr Expr 
           | If Expr Expr Expr
           deriving Show
 
 data Ty = TBool 
-        | TNum 
+        | TNum
+        | TFun Ty Ty
         deriving (Show, Eq)
 
 data Token = TokenNum Int
