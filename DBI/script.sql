0649f95f3927d15bd1ed7450ba2cc2cd8679f373
dreyrobert <dreyrobert@gmail.com>
2024-06-07 14:27:38 -0300

    Trabalho IA

--- DBI/script.sql
+++
@@ -1,4 +0,0 @@
-insert into tarifa (descr,valor) values ('black',0);
-
-insert into locacao (placa,codcli,dtloca,tarifa)
- values ('MK-10',’22’,'15/10/2020','black');
---
+++ DBI/scripts.sql
@@ -0,0 +1,5 @@
+## de filmes
+
+SELECT count(costf) FROM avaliacoes WHERE email = 'cinefilo@gmail.com';
+
+SELECT 
